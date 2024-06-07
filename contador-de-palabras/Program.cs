83bc0ca7e85731623dc8aca58f073b2187488052
Claudia <claudiadurantupano@gmail.com>
2024-06-07 15:28:07 -0400

    contador
    contador de palabras

---
+++ contador-de-palabras/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escribe el parrafo de tu preferencia ");
+string parrafo= Console.ReadLine();
+
+string[] cadena = parrafo.Split(" ");
+int frecuencia = 0;
+Dictionary<string, int> almacenamientoDeCadena = new Dictionary<string, int>() {};
+
+
+foreach (var item in cadena)
+{
+    if (almacenamientoDeCadena.ContainsKey(item))
+    {
+        almacenamientoDeCadena[item]++;
+        frecuencia++;
+    }
+    else{
+        almacenamientoDeCadena[item] = 1;
+    }
+}
+
+foreach (var i in almacenamientoDeCadena)
+{
+    Console.WriteLine($"la palabra: {i.Key}, y su frecuencia es:  {i.Value}");
+}
+
