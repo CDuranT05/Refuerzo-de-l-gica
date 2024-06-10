e771066c95537d93823b3a4c78587303be9c4f05
Claudia <claudiadurantupano@gmail.com>
2024-06-09 23:37:26 -0400

    loteria

---
+++ calculadora/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Bienvenido, por favor ingrese las calificaciones de los estudiantes:");
+List<double> estudiantes = new List<double>();
+
+double usuario;
+usuario = Convert.ToDouble(Console.ReadLine());
+
+while (usuario != 0)
+{
+    if (usuario > 0)
+    {
+        estudiantes.Add(usuario);
+    }
+    Console.WriteLine("escribe 0 cuando ya no quieras agregar estudiantes");
+    usuario = Convert.ToDouble(Console.ReadLine());
+
+}
+
+double calificacionMasAlta = estudiantes.Max();
+double menorCalificacion = estudiantes.Min();
+double Promedio = estudiantes.Sum() / estudiantes.Count();
+
+Console.WriteLine("la calificacion mas alta es: " + calificacionMasAlta);
+Console.WriteLine("la calificacion mas baja es: " + menorCalificacion);
+Console.WriteLine("el promedio de todos los estudiantes es: " + Promedio);
